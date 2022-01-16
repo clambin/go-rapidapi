423d2f9b43260824d46f5977120da4ef9fda7b3e
clambin <christophe.lambin@gmail.com>
2022-01-17 00:53:22 +0100

    fix: improve context / timeout handling

--- client.go
+++ client.go
@@ -3,7 +3,8 @@ package rapidapi
 import (
 	"context"
 	"errors"
-	"io/ioutil"
+	"fmt"
+	"io"
 	"net/http"
 	"time"
 )
@@ -21,10 +22,25 @@ type API interface {
 // If Hostname is set, it will be used to construct the URL and fill in the x-rapidapi-host header field.
 // For unit tests, set the URL field and ignore the Hostname field.
 type Client struct {
-	HTTPClient *http.Client
 	Hostname   string
 	URL        string
 	APIKey     string
+	httpClient *http.Client
+}
+
+// New creates a new client
+func New(hostname, apiKey string) *Client {
+	return &Client{
+		httpClient: http.DefaultClient,
+		Hostname:   hostname,
+		APIKey:     apiKey,
+	}
+}
+
+// WithHTTPClient sets the client's httpClient
+func (client *Client) WithHTTPClient(httpClient *http.Client) *Client {
+	client.httpClient = httpClient
+	return client
 }
 
 // Call an endpoint on the API
@@ -32,18 +48,17 @@ func (client *Client) Call(endpoint string) (body []byte, err error) {
 	return client.CallWithContext(context.Background(), endpoint)
 }
 
-// CallWithContext calls an endpoint on the API with a provided context
-func (client *Client) CallWithContext(ctx context.Context, endpoint string) (body []byte, err error) {
-	httpClient := http.DefaultClient
-	if client.HTTPClient != nil {
-		httpClient = client.HTTPClient
-	}
-
+func (client Client) makeURL(endpoint string) string {
 	baseURL := "https://" + client.Hostname
 	if client.URL != "" {
 		baseURL = client.URL
 	}
-	url := baseURL + endpoint
+	return baseURL + endpoint
+}
+
+// CallWithContext calls an endpoint on the API with a provided context
+func (client *Client) CallWithContext(ctx context.Context, endpoint string) (body []byte, err error) {
+	url := client.makeURL(endpoint)
 
 	const (
 		initWaitTime = 100 * time.Millisecond
@@ -51,36 +66,52 @@ func (client *Client) CallWithContext(ctx context.Context, endpoint string) (bod
 		maxRetry     = 10
 	)
 	waitTime := initWaitTime
-	retries := 0
-	for calling := true; calling && retries < maxRetry; retries++ {
-		req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-		req.Header.Add("x-rapidapi-key", client.APIKey)
-		req.Header.Add("x-rapidapi-host", client.Hostname)
 
-		var resp *http.Response
-		resp, err = httpClient.Do(req)
+	for retries := 0; retries < maxRetry; retries++ {
+		body, err = client.call(ctx, url)
 
-		if err != nil {
+		if err == nil {
 			break
 		}
 
-		switch resp.StatusCode {
-		case http.StatusOK:
-			body, err = ioutil.ReadAll(resp.Body)
-			calling = false
-		case http.StatusTooManyRequests:
-			err = errors.New(resp.Status)
-			time.Sleep(waitTime)
-			if waitTime < maxWaitTime {
-				waitTime *= 2
-			}
-		default:
-			err = errors.New(resp.Status)
-			calling = false
+		if err.Error() != fmt.Sprintf("%3d %s", http.StatusTooManyRequests, http.StatusText(http.StatusTooManyRequests)) {
+			break
 		}
 
-		_ = resp.Body.Close()
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-time.After(waitTime):
+			break
+		}
+
+		if waitTime < maxWaitTime {
+			waitTime *= 2
+		}
 	}
 
 	return
+}
+
+func (client *Client) call(ctx context.Context, url string) (body []byte, err error) {
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	req.Header.Add("x-rapidapi-key", client.APIKey)
+	req.Header.Add("x-rapidapi-host", client.Hostname)
+
+	var resp *http.Response
+	resp, err = client.httpClient.Do(req)
+
+	if err != nil {
+		return
+	}
+
+	defer func() {
+		_ = resp.Body.Close()
+	}()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+
+	return io.ReadAll(resp.Body)
 }
--- client_test.go
+++ client_test.go
@@ -1,6 +1,7 @@
 package rapidapi_test
 
 import (
+	"context"
 	"github.com/clambin/go-rapidapi"
 	"github.com/clambin/go-rapidapi/stub"
 	"github.com/stretchr/testify/assert"
@@ -31,13 +32,9 @@ func TestClient_Call(t *testing.T) {
 		{"bad endpoint", false, "1234", "/invalid", "404 Not Found"},
 	}
 
-	client := rapidapi.Client{
-		HTTPClient: &http.Client{},
-		URL:        testServer.URL,
-	}
-
 	for _, testCase := range testCases {
-		client.APIKey = testCase.apikey
+		client := rapidapi.New("", testCase.apikey)
+		client.URL = testServer.URL
 		response, err := client.Call(testCase.endpoint)
 
 		if testCase.pass == true {
@@ -61,14 +58,12 @@ func TestClient_Call_TimeOut(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(server.Handle))
 	defer testServer.Close()
 
-	client := rapidapi.Client{
-		HTTPClient: &http.Client{Timeout: 100 * time.Millisecond},
-		URL:        testServer.URL,
-		APIKey:     "1234",
-	}
+	client := rapidapi.New("", "1234").WithHTTPClient(&http.Client{Timeout: 100 * time.Millisecond})
+	client.URL = testServer.URL
 
 	_, err := client.Call("/timeout")
-	assert.Error(t, err)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "Client.Timeout exceeded while awaiting headers")
 }
 
 func TestClient_Call_TooManyRequests(t *testing.T) {
@@ -79,15 +74,32 @@ func TestClient_Call_TooManyRequests(t *testing.T) {
 	testServer := httptest.NewServer(http.HandlerFunc(server.Handle))
 	defer testServer.Close()
 
-	client := rapidapi.Client{
-		HTTPClient: &http.Client{Timeout: 10 * time.Second},
-		URL:        testServer.URL,
-		APIKey:     "1234",
-	}
+	client := rapidapi.New("", "1234").WithHTTPClient(&http.Client{Timeout: 10 * time.Second})
+	client.URL = testServer.URL
 
 	_, err := client.Call("/retry")
 	require.NoError(t, err)
 	assert.Greater(t, server.Called["/retry"], 1)
+}
+
+func TestClient_Call_TooManyRequests_ContextExceeded(t *testing.T) {
+	server := stub.Server{
+		APIKey:    "1234",
+		Processor: Processor,
+	}
+	testServer := httptest.NewServer(http.HandlerFunc(server.Handle))
+	defer testServer.Close()
+
+	client := rapidapi.New("", "1234")
+	client.URL = testServer.URL
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	_, err := client.CallWithContext(ctx, "/longretry")
+	require.Error(t, err)
+	assert.Equal(t, "context deadline exceeded", err.Error())
+	assert.Greater(t, server.Called["/longretry"], 1)
 }
 
 var first = true
@@ -96,6 +108,8 @@ func Processor(w http.ResponseWriter, req *http.Request) {
 	switch req.URL.Path {
 	case "/":
 		_, _ = w.Write([]byte("OK"))
+	case "/timeout":
+		time.Sleep(time.Second)
 	case "/retry":
 		if first {
 			first = false
@@ -103,6 +117,8 @@ func Processor(w http.ResponseWriter, req *http.Request) {
 		} else {
 			_, _ = w.Write([]byte("OK"))
 		}
+	case "/longretry":
+		http.Error(w, "slow down!", http.StatusTooManyRequests)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
